87cdd5864b11bd6ad882e29f489ecd3b2bd6830d
Tofts <ssugavanesh2398@gmail.com>
2020-09-05 00:02:31 +0530

    add all files

---
+++ src/main/java/io/tofts/imagemetadataprocessor/ImageMetadataProcessor.java
@@ -0,0 +1,11 @@
+package io.tofts.imagemetadataprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageMetadataProcessor {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageMetadataProcessor.class,args);
+    }
+}
---
+++ src/main/java/io/tofts/imagemetadataprocessor/controller/ImageMetadataProcessorController.java
@@ -0,0 +1,44 @@
+package io.tofts.imagemetadataprocessor.controller;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class ImageMetadataProcessorController {
+    HashMap<String, List<String>> metadata=new HashMap<>();
+
+    @GetMapping("/getimagemetadata")
+    public HashMap<String,List<String>> GetImageMetadata(@RequestBody String filePath) throws IOException, ImageProcessingException, JSONException {
+        System.out.println("%%%%%%%%%%"+filePath);
+        JSONObject json=new JSONObject(filePath);
+        InputStream imageFile = new FileInputStream(json.getString("filePath"));
+        Metadata meta= ImageMetadataReader.readMetadata(imageFile);
+        for (Directory directory : meta.getDirectories()){
+            System.out.println("  directory=" + directory);
+            List<String> listTag=new ArrayList<>();
+            for (Tag tag : directory.getTags()) {
+                System.out.println("    tag=" + tag);
+                listTag.add(tag.toString());
+            }
+            metadata.put(directory.toString(),listTag);
+        }
+
+        return metadata;
+    }
+}
