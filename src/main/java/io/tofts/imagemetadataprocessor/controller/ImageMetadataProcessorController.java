cea344fe0f56da6d9fe5961f3c7861f7e6d13236
Tofts <ssugavanesh2398@gmail.com>
2020-09-05 13:52:25 +0530

    revert the changed

--- src/main/java/io/tofts/imagemetadataprocessor/controller/ImageMetadataProcessorController.java
+++ src/main/java/io/tofts/imagemetadataprocessor/controller/ImageMetadataProcessorController.java
@@ -1,5 +1,7 @@
 package io.tofts.imagemetadataprocessor.controller;
 
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
@@ -19,41 +21,32 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-
 @RestController
 public class ImageMetadataProcessorController {
-    HashMap<String, String> metadata=new HashMap<>();
+    HashMap<String, List<String>> metadata=new HashMap<>();
 
     @PostMapping("/imagemetadataprocessor")
-    public HashMap<String, String> GetImageMetadata(@RequestParam("file") String filePath) throws IOException, ImageProcessingException, JSONException {
+    public HashMap<String, List<String>> GetImageMetadata(@RequestParam("file") MultipartFile filePath) throws IOException, ImageProcessingException, JSONException {
+        BasicAWSCredentials creds = new BasicAWSCredentials("AKIAWH4BFBSOXX2FT5TE", "2dNR42V1bQYsK0YRfxirXkBAU49o2XYXJIAa7q0u");
 
-        System.out.println("%%%%%%%%%%"+filePath);
-        File file=new File(filePath);
-        //File convFile = new File( filePath.getOriginalFilename() );
-        //FileOutputStream fos = new FileOutputStream( convFile );
-        //fos.write( filePath.getBytes() );
-        //fos.close();
-        //System.out.println("%%%%%%%%%%"+convFile.getName());
-        String[] contents = file.list();
-        System.out.println(contents.length);
-        if (contents != null) {
-            for (String content : contents) {
-                if (content.contains(".jpg"))
-                    metadata.put(content,content);
-                    //log.info("Files with extension jpg" + content);
+        AmazonS3Client s3 = new AmazonS3Client(creds);
+        System.out.println("%%%%%%%%%%");
+        File convFile = new File( filePath.getOriginalFilename() );
+        FileOutputStream fos = new FileOutputStream( convFile );
+        fos.write( filePath.getBytes() );
+        fos.close();
+        System.out.println("%%%%%%%%%%"+convFile.getName());
+        s3.putObject("image-store-metadata",convFile.getName(),convFile);
+        Metadata meta= ImageMetadataReader.readMetadata(convFile);
+        for (Directory directory : meta.getDirectories()){
+            System.out.println("  directory=" + directory);
+            List<String> listTag=new ArrayList<>();
+            for (Tag tag : directory.getTags()) {
+                System.out.println("    tag=" + tag);
+                listTag.add(tag.toString());
             }
-
+            metadata.put(directory.toString(),listTag);
         }
-//        Metadata meta= ImageMetadataReader.readMetadata(file);
-//        for (Directory directory : meta.getDirectories()){
-//            System.out.println("  directory=" + directory);
-//            List<String> listTag=new ArrayList<>();
-//            for (Tag tag : directory.getTags()) {
-//                System.out.println("    tag=" + tag);
-//                listTag.add(tag.toString());
-//            }
-//            metadata.put(directory.toString(),listTag);
-//        }
 
         return metadata;
     }
